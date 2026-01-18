d0e5859c7c81798fcf49c42c7d7139a459ae6cfd
PC4\Dario <oliveridario89@gmail.com>
2026-01-18 12:40:09 +0100

    Randomized Space Saving Counter
    Added simple implementation for SavingSpace counter algorithm to count in example occurrence of Ngrams even on PC with few RAM. It uses a priority queue from a external library

---
+++ GrannySolution/src/GrannyAlgos/Class1.cs
@@ -0,0 +1,16 @@
+/*
+   Author: Dario Oliveri ( https://github.com/Darelbi )
+   Copyright (c) 2026 Dario Oliveri
+   License: MIT (see LICENSE file in repository root for more detail)
+   Original Project: https://github.com/Darelbi/Grammar-Navy */
+namespace GrannyAlgos
+{
+    /// <summary>
+    /// this class does nothing. Its just used as a trick to reference the assemby
+    /// from reflection code.
+    /// </summary>
+    public class ReferenceMe
+    {
+
+    }
+}
---
+++ GrannySolution/src/GrannyAlgos/Containers/IRandSpaceSavingCounter.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrannyAlgos.Containers
+{
+    public interface IRandSpaceSavingCounter<TVal>: ICollection<TVal>
+    {
+    }
+}
